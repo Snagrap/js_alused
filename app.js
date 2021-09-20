6c9b3aab72e531bb81b67c9cd4bf64841ec7b389
Snagrap <karlkarilaid04@gmail.com>
2021-09-20 11:25:37 +0300

    JS variables

---
+++ app.js
@@ -0,0 +1,37 @@
+// Muutujad
+// var,let, const
+// var == let OMG OMGO MOGM OGMO MGOMO GMOMG
+// var == 0 :((((())))) let == 1 BURH URHUBUR
+// CONST Am,ognus
+
+//deklareerimine
+//let name = "Karl Karilaid";
+// KAsutamine
+//console.log(name);
+//name = "mATI KAIRLAID";
+//console.log(name);
+
+// lubatud kõik märgid nime loomisel - tähed, numbrid, _, $
+// numbriga ei saa
+//let $="dollar";
+//console.log($);
+
+const name = "Karl Karilaid";
+console.log(name);
+
+// const ei luba väärtust muuta bruh buruhbu
+//name = "Mati Karilaid"
+
+const numbers = [1,2,3,4,5,6,7,8,9];
+numbers.push(0);
+console.log(numbers);
+
+const person = {
+	name: "Kadi",
+	age: 568
+};
+
+person.name = "Kati";
+person.age = 569
+
+console.log(person);
