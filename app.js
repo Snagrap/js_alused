e93178252bfd40e346b020afcf4a66c3362f2279
Snagrap <karlkarilaid04@gmail.com>
2021-09-24 09:03:01 +0300

    Html testing 2

--- app.js
+++ app.js
@@ -5,12 +5,25 @@ const city = "Tartu";
 
 let html;
 
+// es5
+
 html = "<ul>" +
 			"<li>" + name + "</li>" +
 			"<li>" + age + "</li>" +
 			"<li>" + job + "</li>" +
 			"<li>" + city + "</li>" +
 		"</ul>"
+
+// es6 = template strings
+
+html = 
+	`<ul>
+		<li>Name; ${name}</li>
+		<li>Age; ${age}</li>
+		<li>Job; ${job}</li>
+		<li>City; ${city}</li>
+	<ul>`
+
 
 console.log(html);
 
