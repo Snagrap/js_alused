5904e68cd19d450f34ebdfaccc15edcb6a009446
Snagrap <karlkarilaid04@gmail.com>
2021-09-24 09:41:27 +0300

    Array testing

--- app.js
+++ app.js
@@ -1,30 +1,26 @@
-const name = "Karl Karilaid";
-const age = 17;
-const job = "McDonald's klienditeenindaja";
-const city = "Tartu";
+const numbers1 = [2, 6, 7, 5128591028509182095, 9, 5, 124, 561, 7231, 76553, 71, 2, 1];
+const numbers2 = new Array(741, 425, 45666, 54, 6);
+const dailydiet = ["koeratoit", "ahvliha", "punane hiinlane"];
+const mixed = [895, true, "kuulilennuteetunneliluuk", undefined, null, {a:1,b:2}, new Date()];
+const fruits = ["õun", "banaan", "mango", "apelsin"];
 
-let html;
+let val
 
-// es5
+val = numbers1.length;
+val = Array.isArray(numbers1);
+val = numbers1[3];
+numbers1[2] = 100;
+val = numbers1.indexOf(124);
+val = numbers1.push(12);
+val = numbers1.unshift(2603);
+val = numbers1.pop(125);
+//val = numbers1.splice(1,3);
+//numbers1.reverse();
+val = numbers1.concat(numbers2);
+val = fruits.sort();
+val = numbers1.sort(function(x, y){
+	return y-x;
+});
 
-html = "<ul>" +
-			"<li>" + name + "</li>" +
-			"<li>" + age + "</li>" +
-			"<li>" + job + "</li>" +
-			"<li>" + city + "</li>" +
-		"</ul>"
-
-// es6 = template strings
-
-html = 
-	`<ul>
-		<li>Name; ${name}</li>
-		<li>Age; ${age}</li>
-		<li>Job; ${job}</li>
-		<li>City; ${city}</li>
-	<ul>`
-
-
-console.log(html);
-
-document.body.innerHTML = html
+console.log(val);
+console.log(numbers1);
