b203b46ce50eb0e3fcc97442638944e9074821d1
Snagrap <karlkarilaid04@gmail.com>
2021-09-27 11:43:02 +0300

    Bruh

--- app.js
+++ app.js
@@ -1,79 +1,52 @@
 /*
-if(tingimus) {
-	//tegevus if puhul
+const color = "red";
 
-} else {
-	// tegevused else puhul
-}
-*/
-
-const id = 100;
-
-// == - võrdub
-// != - ei võrdu
-// === - võrdub ja tüübikontroll
-// !== - EI VÕRDU ja tüübikontroll
-
-/*
-let testID;
-
-console.log(testID);
-
-
-if(typeof testID !== 'undefined'){
-	console.log(`ID on ${testID}`);
-} else {
-	console.log("ID puudub");
-}
-
-
-
-if(id > 100) {
-	console.log(`ID on ${id} - suurem kui 100`);
-} else {
-	console.log(`ID on ${id} - väiksem või võrdne kui 100`);
+switch(color) {
+	case "red":
+		console.log("vented");
+		break;
+	case "yellow":
+		console.log("problem? *troll*");
+		break;
+	case "green":
+		console.log(`:"i am not sus"`);
+		break;
+	case "blue":
+		console.log("kuse");
+		break;
+	case "purple":
+		console.log("said amogus");
+		break;
+	default:
+		console.log("Ummmm see ei ole värv :)");
+		break;
 }
 
 */
+const today = new Date();
 
-// if - else if - else
+nädalapäev = today.getDay();
 
-/*
-const color = "green";
-
-if(color === "red") {
-	console.log("stop");
-} else if(color === "yellow") {
-	console.log("valmis");
-} else if(color === "green") {
-	console.log("minek");
-} else {
-	console.log("kuse täis end");
+switch(nädalapäev) {
+	case 1:
+		console.log("Esmaspäev");
+		break;
+	case 2:
+		console.log("Teisipäev");
+		break;
+	case 3:
+		console.log("Kolmapäev");
+		break;
+	case 4:
+		console.log("Neljapäev");
+		break;
+	case 5:
+		console.log("Reede");
+		break;
+	case 6:
+		console.log("Laupäev");
+		break;
+	case 0:
+		console.log("Pühapäev");
+		break;
 }
-*/
-// and - &&
-const name = "mati";
-const age = "73";
-
-if(age > 8 && age <= 16) {
-	console.log(`${name} ei ole veel vangis`);
-} else if(age > 16 && age < 18){
-	console.log(`${name} on valmis kohtusse minekuks`);
-} else {
-	console.log(`${name} on vangis`);
-}
-
-// or - ||
-
-if(age < 18 || age > 65) {
-	console.log(`${name} saab vangis tööd`);
-} else {
-	console.log(`${name} ei saa vangis tööd`);
-}
-
-// ? - ternary operator
-console.log(id === 100 ? "Õige!" : "Vale!");
-
-// ilma {}
-if(id === 100) console.log("Korras :)");
-else console.log("Ei ole koraas !!!! :netural:face:");
