fc6b8bfa0db48ac201ad0cc72f2598da82fddd12
Snagrap <karlkarilaid04@gmail.com>
2021-09-28 09:17:33 +0300

    Loops

--- app.js
+++ app.js
@@ -1,52 +1,57 @@
-/*
-const color = "red";
+// for tsükkel
 
-switch(color) {
-	case "red":
-		console.log("vented");
-		break;
-	case "yellow":
-		console.log("problem? *troll*");
-		break;
-	case "green":
-		console.log(`:"i am not sus"`);
-		break;
-	case "blue":
-		console.log("kuse");
-		break;
-	case "purple":
-		console.log("said amogus");
-		break;
-	default:
-		console.log("Ummmm see ei ole värv :)");
-		break;
+console.log("For tsükkel");
+for(let i = 0; i < 10; i++) {
+	if(i==5) {
+		//break;
+		continue;
+	}
+	console.log(i);
 }
 
-*/
-const today = new Date();
+//while tsükkel :D
 
-nädalapäev = today.getDay();
+console.log("While tsükkel");
+let i=0;
+while(i<10){
+	console.log(i);
+	i++;
+}
 
-switch(nädalapäev) {
-	case 1:
-		console.log("Esmaspäev");
-		break;
-	case 2:
-		console.log("Teisipäev");
-		break;
-	case 3:
-		console.log("Kolmapäev");
-		break;
-	case 4:
-		console.log("Neljapäev");
-		break;
-	case 5:
-		console.log("Reede");
-		break;
-	case 6:
-		console.log("Laupäev");
-		break;
-	case 0:
-		console.log("Pühapäev");
-		break;
+//do while tsükkel :(
+
+console.log("Do while tsükkel");
+let j=0;
+do{
+	console.log(j);
+	j++;
+} while(j<10);
+
+//tsüklid ja massiivid
+console.log("Massivid ja tsüklid");
+//for tsükliga
+const cars=["Mersu","Bmw","Honda"];
+for(let i=0; i<cars.length; i++){
+	console.log(cars[i]);
+}
+//forEach
+console.log("ForEach function");
+cars.forEach(function(element, index){
+	console.log(`cars[${index}] = ${element}`);
+});
+
+//forEach (sus editon)
+console.log("ForEach function, but sus.");
+cars.forEach((element, index) => {
+	console.log(`cars[${index}] = ${element}`);
+});
+console.log();
+//tsüklid ja objektid
+const crewmate={ 
+	color:"Blue",
+	impostor: false,
+	susMeter: 44
+};
+for(let key in crewmate){
+	console.log(`${key}=${crewmate[key]}`);
 }
