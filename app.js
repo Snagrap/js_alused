df737100bb24ec940d3027af29e5758989c393ab
Snagrap <karlkarilaid04@gmail.com>
2021-09-28 10:20:30 +0300

    MA ei saa hakkama

--- app.js
+++ app.js
@@ -1,37 +1,3 @@
-//funktsioonid defineerimine (deklareerimine)
-function greeting(firstname="nimetu", lastname="härrasmees"){
-	/*if(typeof firstname==="undefined"){
-		firstname="Saksasus"
-	}
-	if(typeof lastname==="undefined"){
-		lastname="Lembirt"
-	} */
-	return "tere "+firstname+" "+lastname;
+function amog(){
+	console.log();
 }
-//funktsioonid kutsumine tööle
-let greet=greeting("sussy", "baka");
-console.log(greet);
-const square=function(number=3){
-	return number*number;
-}
-console.log(square());
-(function(){
-	console.log("Function is runningi nginginginin!!!!!")
-})();
-(function(name){
-	console.log("Function is running! "+"Tere "+name);
-})("Albert");
-const todo={
-	add:function(){
-		console.log("See on cringe fucntion mida ma ei hakka kuangi oma eluis kasutama))))");
-	},
-	edit:function(id){
-		console.log(`èdit todo nr ${id} ...`);
-	},
-	delete:function(){
-		console.log("delete todo...");
-	}
-}
-todo.add();
-todo.edit("karl karilaid");
-todo.delete();
