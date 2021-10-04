b2683e198ab0fcc91c5d59138904c960fc9e3674
Snagrap <karlkarilaid04@gmail.com>
2021-10-04 11:44:32 +0300

    work in progress task remover v1

--- app.js
+++ app.js
@@ -1,30 +1,6 @@
-const form=document.querySelector("form");
-const taskInput=document.querySelector("#task");
-const heading=document.querySelector("h4");
-const li=document.createElement("li");
-const list=document.querySelector("ul");
-
-li.className="collection-item";
-
-taskInput.value = "";
-
-form.addEventListener("submit", addTask);
-//taskInput.addEventListener("keydown", runEvent);
-//taskInput.addEventListener("keyup", runEvent);
-//taskInput.addEventListener("keypress", runEvent);
-//taskInput.addEventListener("focus", runEvent);
-//taskInput.addEventListener("blur", runEvent);
-//taskInput.addEventListener("cut", runEvent);
-//taskInput.addEventListener("paste", runEvent);
-//taskInput.addEventListener("input", runEvent);
-
-function addTask(e){
-	li.appendChild(document.createTextNode(taskInput.value));
-	const link=document.createElement("a");
-	link.className="secondary-content";
-	link.appendChild(document.createTextNode("X"));
-	link.setAttribute("href", "#");
-	li.appendChild(link);
-	list.appendChild(li);
-	e.preventDefault();
+const taskList=document.querySelector("ul")
+console.log(a.parentElement);
+taskList.addEventListener("click", runEvent);
+function runEvent(e){
+	console.log(`Event is ${e.type}`);
 }
