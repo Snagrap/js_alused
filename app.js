22402e94f67fee005cb8093af723e04cd850bdda
Snagrap <karlkarilaid04@gmail.com>
2021-10-13 10:20:37 +0300

    Added delete all button

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
-const taskList=document.querySelector("ul")
-console.log(a.parentElement);
-taskList.addEventListener("click", runEvent);
-function runEvent(e){
-	console.log(`Event is ${e.type}`);
+// event elements
+const taskList = document.querySelector('ul');
+const deleteAllButton = document.getElementById('delete-all');
+
+// click elemendi kustutamiseks
+taskList.addEventListener("click", deleteTask);
+deleteAllButton.addEventListener("click", deleteAll);
+
+function deleteTask(e) {
+if(e.target.textContent === "X") {
+if(confirm("Are you sure you want to delete this task?")){
+e.target.parentElement.remove();
+		}
+	}
+}
+
+
+function deleteAll(e) {
+if(e.target.id === "delete-all"){
+if(confirm("Are you sure you want to delete all tasks?")){
+taskList.innerHTML = "";
+		}
+	}
 }
