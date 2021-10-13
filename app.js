2a15096fd576d3d736d1e202188321e196ae52e5
Snagrap <karlkarilaid04@gmail.com>
2021-10-13 10:59:14 +0300

    WOrking TO-DO adder v5

--- app.js
+++ app.js
@@ -22,3 +22,29 @@ taskList.innerHTML = "";
 		}
 	}
 }
+
+const form = document.querySelector('form');
+const taskInput = document.querySelector('#task');
+
+// submit
+form.addEventListener('submit', addTask);
+
+function addTask(e) {
+	const li = document.createElement('li');
+	const list = document.querySelector('ul');
+	
+	li.className = 'collection-item';
+	li.appendChild(document.createTextNode(taskInput.value));
+
+	const link = document.createElement('a');
+	link.className = 'secondary-content';
+	link.appendChild(document.createTextNode('X'));
+	link.setAttribute('href', '#');
+
+	li.appendChild(link);
+	list.appendChild(li);
+
+	taskInput.value = '';
+
+	e.preventDefault();
+}
